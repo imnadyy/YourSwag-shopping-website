096d9e968f106de44f7278848fe7cae5444620fa
Nadeem <nadeemali581@gmail.com>
2021-07-26 20:59:49 +0530

    fixed issue in hero file

--- src/components/pages/landingPage/hero/hero.jsx
+++ src/components/pages/landingPage/hero/hero.jsx
@@ -3,11 +3,6 @@ import './hero.css';
 import Content from './content/content';
 
 class Hero extends Component {
-<<<<<<< HEAD:src/components/pages/landingPage/hero/hero.jsx
-        
-=======
-    
->>>>>>> 273f5af9f97ad2568c2445119fb593f6d9305118:src/components/hero/hero.jsx
     render() { 
         return ( 
         <div className='hero'>
